44a0138faa86d46fe15992cf2df4cc78c93232d1
mcv1993 <mcv1993@gmail.com>
2019-08-14 01:30:21 +0000

    BeforeRedesign

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course, only: [:show]
+
   def show
   end
 
@@ -8,5 +11,12 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_enrolled_for_current_course
+    if current_lesson.section.course.enrollment.user
+      redirect_to course_path(current_lesson.section.course), alert: 'Must be enrolled'
+    end
+  end
+
 
 end
