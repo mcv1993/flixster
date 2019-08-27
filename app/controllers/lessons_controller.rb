38e72a3d8a059297a463bc5d554361469cf4cba2
mcv1993 <mcv1993@gmail.com>
2019-08-27 03:18:00 +0000

    L30

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,16 +7,15 @@ class LessonsController < ApplicationController
 
   private
 
-  helper_method :current_lesson
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
-
   def require_enrolled_for_current_course
-    if current_lesson.section.course.enrollment.user
+    if current_lesson.section.course.user.enrolled_in?(current_lesson.section.course) != current_user
       redirect_to course_path(current_lesson.section.course), alert: 'Must be enrolled'
     end
   end
 
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 
 end
