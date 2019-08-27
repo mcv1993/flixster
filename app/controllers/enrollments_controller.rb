40214bed9a0100488e5c263ab3b91978018add27
mcv1993 <mcv1993@gmail.com>
2019-08-27 00:11:38 +0000

    L29

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,10 +2,10 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.enrollments.create(course: current_course)
+    if current_course.premium?
 
         # Amount in cents
-        @amount = 500
+        @amount = (current_courses.cost * 100).to_i
 
         customer = Stripe::Customer.create(
           email: params[:stripeEmail],
@@ -15,10 +15,11 @@ class EnrollmentsController < ApplicationController
         charge = Stripe::Charge.create(
           customer: customer.id,
           amount: @amount,
-          description: 'Rails Stripe customer',
+          description: 'Flixster Content',
           currency: 'usd'
         )
-    
+    end
+      current_user.enrollments.create(course: current_course)
       redirect_to course_path(current_course)
       rescue Stripe::CardError => e
         flash[:error] = e.message
