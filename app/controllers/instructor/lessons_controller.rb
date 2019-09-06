f3772c4cb209cbbcc26fbca8a65de8a2c6406acd
mcv1993 <mcv1993@gmail.com>
2019-09-06 04:31:56 +0000

    L36

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,12 +1,8 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_user_section, only: [:new, :create]
+  before_action :require_authorized_for_current_user_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
   protect_from_forgery with: :null_session
-
-  def new
-    @lesson = Lesson.new
-  end
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
